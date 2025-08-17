6b802a5ecb4dba4ab7b4bf5ea06a6efc88cd9b50
benlowesofficial-hub <benlowesofficial@gmail.com>
2025-08-17 15:26:55 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        paper: "#FFFCF2",
+        charcoal: "#333333",
+        pebbleTeal: "#96B6B6",
+        mutedRed: "#D65A4A",
+        softYellow: "#E9C46A",
+        gentleBlue: "#6CA6C1"
+      },
+      fontFamily: {
+        sans: ["Nunito", "system-ui", "sans-serif"]
+      },
+      borderRadius: {
+        xl2: "1.25rem"
+      }
+    }
+  },
+  plugins: []
+}
